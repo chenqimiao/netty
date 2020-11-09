6ee2991c22f9520af60c5f8f71752664f223c4a3
Qimiao Chen <chenqimiao1994@126.com>
2020-11-09 19:37:00 +0800

    通用模式的 NIO 实现跨平台的多路复用器

--- transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
+++ transport/src/main/java/io/netty/channel/socket/nio/NioServerSocketChannel.java
@@ -47,6 +47,7 @@ public class NioServerSocketChannel extends AbstractNioMessageChannel
                              implements io.netty.channel.socket.ServerSocketChannel {
 
     private static final ChannelMetadata METADATA = new ChannelMetadata(false, 16);
+    // 利用 jdk 实现跨平台的 IO 多路复用
     private static final SelectorProvider DEFAULT_SELECTOR_PROVIDER = SelectorProvider.provider();
 
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(NioServerSocketChannel.class);
